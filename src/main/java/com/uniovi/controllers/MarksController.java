a60f2c016fed3659e619d18de359ac52396b6c65
Nerea Valdés Egocheaga <nerevalego@gmail.com>
2020-02-20 15:31:56 +0100

    SDI-SpringBoot-Sesión-4.1-Sesiones

--- src/main/java/com/uniovi/controllers/MarksController.java
+++ src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,5 +1,10 @@
 package com.uniovi.controllers;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/uniovi/services/MarksService.java
+++ src/main/java/com/uniovi/services/MarksService.java
@@ -1,7 +1,11 @@
 package com.uniovi.services;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +18,7 @@ public class MarksService {
 
 	@Autowired
 	private MarksRepository marksRepository;
-
+	
 	public List<Mark> getMarks() {
 		List<Mark> marks = new ArrayList<Mark>();
 		marksRepository.findAll().forEach(marks::add);
@@ -22,7 +26,8 @@ public class MarksService {
 	}
 
 	public Mark getMark(Long id) {
-		return marksRepository.findById(id).get();
+		Mark obtainedmark = marksRepository.findById(id).get();
+		return obtainedmark;
 	}
 
 	public void addMark(Mark mark) {
